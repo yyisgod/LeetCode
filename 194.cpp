eadea6fe42891e08f80cc68e57965f1806625c92
yyisgod <yy251745080@126.com>
2016-08-02 14:11:35 +0800

    194

--- 194.cpp
+++ 194.cpp
@@ -123,3 +123,32 @@ int main() {
 	Solution s;
 	cout << s.maxProfit(1,b);
 }
+/*
+ * int maxProfit(int k, vector<int>& prices) {
+    int n = (int)prices.size();
+    if (n <= 1) return 0;
+    int fm = 0, km = 0;
+    for (int j = 1; j < n; j++) {
+        int f = prices[j]-prices[j-1];
+        if (f>0) {
+            fm += f;
+            km++;
+        }
+    }
+    if (k >= km) {
+        return fm;
+    } else {
+        int f[n], prev;
+        fill_n(f, n, 0);
+        for (int i = 1; i <= k; i++) {
+            int S = INT_MIN;
+            prev = f[0];
+            for (int j = 1; j < n; j++) {
+                S = std::max(S, prev-prices[j-1]);
+                prev = f[j];
+                f[j] = std::max(f[j-1], S+prices[j]);
+            }
+        }
+        return f[n-1];
+    }
+	*/
